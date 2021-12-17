514ea29a9c7f8b8c9427da4acc96aa38c662f1b6
SeongHyeokShin <sin4191@gmail.com>
2021-12-17 10:48:14 +0900

    Setup main
    실행될 자바 메인 진입점 만들기

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void main (){
+        //Given
+        String [] args = {"3","2","4"};
+
+        //When
+        Main.main(args);
+
+        //Then
+    }
+}
